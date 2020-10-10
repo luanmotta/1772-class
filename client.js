f6c49228bfe8bdee69fbc5f7c6b093c1c1f5370b
luanmotta <luanmotta97@hotmail.com>
2020-10-10 17:28:29 -0300

    Update

--- client.js
+++ client.js
@@ -24,27 +24,54 @@ Frase: Oi, eu sou Luan Motta, tenho 23 anos, e sou Escritor(a) Tagarela Que esqu
 
 */
 
+const http = require('http')
 const https = require('https')
 const whoAreYou = require('./whoAreYou')
-const urls = ['https://eecc3e35f9f3.ngrok.io']
+const regexUrl = /((([A-Za-z]{3,9}:(?:\/\/)?)(?:[-;:&=\+\$,\w]+@)?[A-Za-z0-9.-]+|(?:www.|[-;:&=\+\$,\w]+@)[A-Za-z0-9.-]+)((?:\/[\+~%\/.\w-_]*)?\??(?:[-\+=&;%@.\w_]*)#?(?:[\w]*))?)/
+const SERVER_PORT = 8080
+
+var urls = []
+var urlsFiltered = []
 var responses = [];
 var completed_requests = 0;
 
-for (i in urls) {
-  https.get(urls[i], (res) => {
-    let data = ''
-    res.on('data', chunk => {
-      data += chunk
-    })
-
-    res.on('end', function(){
-      responses.push(JSON.parse(data))
-      if (completed_requests++ === urls.length - 1) {
-        // All downloads are completed
-        mountObjects()
-      }
-    })
+const googleSheetsURL =  'https://sheets.googleapis.com/v4/spreadsheets/13eOFLQTxo121a7CwJ-lAUmgDAxZJ5go1i9t8-dWUUW0/values/B3:B99?key=AIzaSyB_krgCvGveFrsA26mnR8AMFCKcP0QEeGo';
+https.get(googleSheetsURL, (res) => {
+  let data = ''
+  res.on('data', chunk => {
+    data += chunk
   })
+
+  res.on('end', () => {
+    urls = JSON.parse(data).values
+    urls.filter(arrayUrl => {
+      if (arrayUrl[0] && arrayUrl[0].match(regexUrl)) urlsFiltered.push(arrayUrl[0])
+    })
+    doRequests()
+  })
+})
+
+const doRequests = () => {
+  for (i in urlsFiltered) {
+    https.get(urlsFiltered[i], (res) => {
+      let data = ''
+      res.on('error', error => {
+        throw error
+      })
+      res.on('data', chunk => {
+        data += chunk
+      })
+      res.on('end', () => {
+        if (!data.includes('not found')) {
+          responses.push(JSON.parse(data))
+        }
+        if (completed_requests++ === urlsFiltered.length - 1) {
+          // All downloads are completed
+          mountObjects()
+        }
+      })
+    })
+  }
 }
 
 const mountPhrase = (obj) => {
@@ -55,9 +82,12 @@ const mountObjects = () => {
   responses.forEach(obj => {
     obj.frase = mountPhrase(obj)
   })
-  console.log(responses)
+  renderPage()
 }
 
 const renderPage = () => {
-
+  http.createServer((req, res) => {
+    res.write(JSON.stringify(responses))
+    res.end()
+  }).listen(SERVER_PORT, () => console.log(`Server running on port ${SERVER_PORT}`))
 }
