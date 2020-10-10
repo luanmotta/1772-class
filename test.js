7dc1c7858ea76ebeab0ae8b51d1c4fc2c69fbcc9
Guilherme Luvielmo <luvielmo.guilherme@gmail.com>
2020-10-10 17:08:46 -0300

    wip

--- test.js
+++ test.js
@@ -2,6 +2,8 @@ const https = require('https')
 
 const url =  'https://sheets.googleapis.com/v4/spreadsheets/13eOFLQTxo121a7CwJ-lAUmgDAxZJ5go1i9t8-dWUUW0/values/A3:A5?key=AIzaSyB_krgCvGveFrsA26mnR8AMFCKcP0QEeGo';
 
+let values;
+
 https.get(url, (res) => {
   let data = ''
   res.on('data', chunk => {
@@ -9,6 +11,6 @@ https.get(url, (res) => {
   })
 
   res.on('end', function(){
-    console.log(data);
+    console.log(JSON.parse(data).values);
   })
 })
